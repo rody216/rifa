afc0a32de2191d937474171ba970ba8b722b1940
rody216 <rody216@gmail.com>
2022-10-28 15:41:29 -0500

    Actualizo datos del servidor

--- db.php
+++ db.php
@@ -1,12 +1,29 @@
 <?php
+
 session_start();
 
 $conn = mysqli_connect(
-  'localhost',
-  'root',
-  '',
+  'us-cdbr-east-06.cleardb.net',
+  'bd26ff67c67b5d',
+  'ff6bbecb',
   // 'php_mysql_crud'
-  'rifa'
+  'heroku_c010fa7b046c9d5'
 ) or die(mysqli_erro($mysqli));
 
+
+// define('SERVIDOR', 'us-cdbr-east-06.cleardb.net');
+// define('NOMBRE_BD', 'heroku_c010fa7b046c9d5');
+// define('USUARIO', 'bd26ff67c67b5d');
+// define('CLAVE', 'ff6bbecb');
+
+// $conn = [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'];
+
+// try {
+//   $pdo = new PDO('mysql:host=' . SERVIDOR . ';dbname=' . NOMBRE_BD, USUARIO, CLAVE, $conn);
+//   return $pdo;
+// } catch (Exception $e) {
+//   $respuesta = ['Error' => 'error: ' . $e->getMessage()];
+//   print json_encode($respuesta);
+//   die();
+// }
 ?>
